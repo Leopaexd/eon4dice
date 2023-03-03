82c5a8580458356c5a85c866b92200db6547bb49
Oliver <oliver.glant@gmail.com>
2023-03-03 18:28:49 +0100

    Initial commit

---
+++ tests/test_eon4dice.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from eon4dice.src.eon4dice import eon4dice
+
+
+class Test(TestCase):
+    def test_roll(self):
+        test_strings = [
+            '5T6+2',
+            '1T6',
+            '1t100',
+            '2T6+0'
+        ]
+        for test in test_strings:
+            assert(eon4dice.roll(test) > 0)
